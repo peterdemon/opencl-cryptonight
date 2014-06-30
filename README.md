6ebe66ebc61cbbbd237d9ec11b82635cc10d7298
Wolf <wolf@mykolab.com>
2014-06-30 15:40:35 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,87 @@
+CPUMiner-Multi
+==============
+
+This is a multi-threaded CPU miner, fork of [LucasJones](//github.com/lucasjones)' cpuminer-multi.
+
+#### Table of contents
+
+* [Algorithms](#algorithms)
+* [Dependencies](#dependencies)
+* [Download](#download)
+* [Build](#build)
+* [Usage instructions](#usage-instructions)
+* [Donations](#donations)
+* [Credits](#credits)
+* [License](#license)
+
+Algorithms
+==========
+#### Currently supported
+ * ✓ __scrypt__ (Litecoin, Dogecoin, Feathercoin, etc..)
+ * ✓ __sha256d__ (Bitcoin, Freicoin, Peercoin/PPCoin, Terracoin, etc..)
+ * ✓ __x11__ (Darkcoin [DRK], Hirocoin, Limecoin)
+ * ✓ __cryptonight__ (Bytecoin [BCN], Monero)
+
+#### Implemented, but untested
+ * ? keccak (Maxcoin  HelixCoin, CryptoMeth, Galleon, 365coin, Slothcoin, BitcointalkCoin)
+ * ? hefty1 (Heavycoin)
+ * ? quark (Quarkcoin)
+ * ? skein (Skeincoin, Myriadcoin)
+ * ? shavite3 (INKcoin)
+ * ? blake (Blakecoin)
+
+Dependencies
+============
+* libcurl			http://curl.haxx.se/libcurl/
+* jansson			http://www.digip.org/jansson/ (jansson is included in-tree)
+* openssl			https://www.openssl.org/
+
+Download
+========
+* Binary releases: None yet
+* Git tree:   https://github.com/wolf9466/cpuminer-multi
+* Clone with `git clone https://github.com/wolf9466/cpuminer-multi`
+
+Build
+=====
+
+#### Basic *nix build instructions:
+ * ./autogen.sh	# only needed if building from git repo
+ * Optimal GCC flags are built in - you only need to use -march=native if you want it
+ * CFLAGS="*-march=native*" ./configure
+   * # Use -march=native if building for a single machine
+ * make
+
+#### Architecture-specific notes:
+ * CryptoNight works only on x86 and x86-64.
+ * If you don't have AES-NI, it's slow. This implementation is deprecated and will not be improved.
+
+Usage instructions
+==================
+Run "minerd --help" to see options.
+
+### Connecting through a proxy
+
+Use the --proxy option.
+
+To use a SOCKS proxy, add a socks4:// or socks5:// prefix to the proxy host  
+Protocols socks4a and socks5h, allowing remote name resolving, are also available since libcurl 7.18.0.
+
+If no protocol is specified, the proxy is assumed to be a HTTP proxy.  
+When the --proxy option is not used, the program honors the http_proxy and all_proxy environment variables.
+
+Donations
+=========
+Donations for the work done in this fork are accepted at
+* MRO: `43jtN2e88d4ayn7gxsLwB94fmYzLuXymuGdJxotnvYsCaK3UuhvQRNgKrZUD4i5kxTRtpz49FrB1e2KYiXu4TNyEEGUwu9E`
+* BTC: `1WoLFumNUvjCgaCyjFzvFrbGfDddYrKNR`
+
+Credits
+=======
+This faster CPUMiner-multi was forked from LucasJones', and has been developed by Wolf.
+Special thanks to Intel for helping me with the usage of the AESKEYGENASSIST instruction, which I used to replace the calls to the slow, unoptimized oaes_lib.
+Special thanks also to dga for his code with that loop - since it runs over 500,000 times per hash, it's extremely important.
+
+License
+=======
+GPLv2.  See COPYING for details.
---
+++ cryptonight_common.c
@@ -0,0 +1,84 @@
+// Copyright (c) 2012-2013 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+// Modified for CPUminer by Lucas Jones
+
+#include "cpuminer-config.h"
+#include "miner.h"
+#include "crypto/c_keccak.h"
+#include "crypto/c_groestl.h"
+#include "crypto/c_blake256.h"
+#include "crypto/c_jh.h"
+#include "crypto/c_skein.h"
+#include "crypto/int-util.h"
+#include "crypto/hash-ops.h"
+#include "cryptonight.h"
+
+#ifdef __unix__
+#include <sys/mman.h>
+#else
+#include <windows.h>
+#endif
+
+void do_blake_hash(const void* input, size_t len, char* output) {
+    blake256_hash((uint8_t*)output, input, len);
+}
+
+void do_groestl_hash(const void* input, size_t len, char* output) {
+    groestl(input, len * 8, (uint8_t*)output);
+}
+
+void do_jh_hash(const void* input, size_t len, char* output) {
+    jh_hash(HASH_SIZE * 8, input, 8 * len, (uint8_t*)output);
+}
+
+void do_skein_hash(const void* input, size_t len, char* output) {
+    skein_hash(8 * HASH_SIZE, input, 8 * len, (uint8_t*)output);
+}
+
+void xor_blocks_dst(const uint8_t *restrict a, const uint8_t *restrict b, uint8_t *restrict dst) {
+    ((uint64_t*) dst)[0] = ((uint64_t*) a)[0] ^ ((uint64_t*) b)[0];
+    ((uint64_t*) dst)[1] = ((uint64_t*) a)[1] ^ ((uint64_t*) b)[1];
+}
+
+void (* const extra_hashes[4])(const void *, size_t, char *) = {do_blake_hash, do_groestl_hash, do_jh_hash, do_skein_hash};
+
+void cryptonight_hash(void* output, const void* input, size_t len) {
+    struct cryptonight_ctx *ctx = (struct cryptonight_ctx*)malloc(sizeof(struct cryptonight_ctx));
+    cryptonight_hash_ctx(output, input, ctx);
+    free(ctx);
+}
+
+__thread struct cryptonight_ctx *persistentctx = NULL;
+
+int scanhash_cryptonight(int thr_id, uint32_t *restrict pdata, const uint32_t *restrict ptarget, uint32_t max_nonce, unsigned long *restrict hashes_done) {
+    uint32_t *nonceptr = (uint32_t*) (((char*)pdata) + 39);
+    uint32_t n = *nonceptr - 1;
+    const uint32_t first_nonce = n + 1;
+    const uint32_t Htarg = ptarget[7];
+    uint32_t hash[HASH_SIZE / 4] __attribute__((aligned(32)));
+	
+	if(!persistentctx)
+	{
+		#ifdef __unix__
+		persistentctx = (struct cryptonight_ctx *)mmap(0, sizeof(struct cryptonight_ctx), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_POPULATE, 0, 0);
+		if(persistentctx == MAP_FAILED) persistentctx = (struct cryptonight_ctx *)malloc(sizeof(struct cryptonight_ctx));
+		#else
+		persistentctx = VirtualAlloc(NULL, sizeof(struct cryptonight_ctx), MEM_LARGE_PAGES, PAGE_READWRITE);
+		if(!persistentctx) persistentctx = (struct cryptonight_ctx *)malloc(sizeof(struct cryptonight_ctx));
+		#endif
+	}
+	
+	do {
+		*nonceptr = ++n;
+		cryptonight_hash_ctx(hash, pdata, persistentctx);
+		if (unlikely(hash[7] < ptarget[7])) {
+			*hashes_done = n - first_nonce + 1;
+			return true;
+		}
+	} while (likely((n <= max_nonce && !work_restart[thr_id].restart)));
+    
+    *hashes_done = n - first_nonce + 1;
+    return 0;
+}
